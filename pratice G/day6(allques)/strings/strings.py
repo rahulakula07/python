66392c46a57d0d11bebf63a283683dafae3a3660
rahul akula <rahulakula4265@gmail.com>
2025-03-18 00:17:53 +0530

    day 6 commited

--- pratice G/day6(allques)/strings/strings.py
+++ pratice G/day6(allques)/strings/strings.py
@@ -43,3 +43,35 @@ print(count)
 # output
 # Hello WORLD
 # 3
+############################################################################
+# Remove Vowels from a String
+#        "hello world" → "hll wrld"
+# Write a function to remove all vowels from a given string.
+
+str=input()
+vowels="aeiouAEIOU"
+res=""
+for char in str:
+    if char not in vowels:
+        res+=char
+print(count)
+# output
+# Hello World
+# Hll Wrld.
+################################################################################
+# Convert String to Title Case
+#        "hello world" → "Hello World"
+# Write a function that converts a string to title case (capitalize the first letter of each word).
+
+str=input()
+print(str.title())
+# output
+# hello world
+# Hello World 
+str1=input().split()
+cap=[word[0].upper()+word[1:] for word in str1]
+print(" ".join(cap))
+# output
+# hello worls
+# Hello Worls
+######################################################################################
